fedf4c4d381dc898342aacd6d308a8cc67cf5b79
carolcabralm <carolcabralm@msn.com>
2022-07-21 11:31:43 -0300

    feat: preparando ambiente para testes

--- src/controllers/ativosController.js
+++ src/controllers/ativosController.js
@@ -6,7 +6,7 @@ const getByCodAtivoOuCliente = async (req, res, next) => {
   if (!isCodAtivo) {
     const codClienteToken = req.user.codCliente;
     if(codClienteToken !== +codigo) return res.status(401).json({ message: 'Não autorizado' });
-    
+
     const { code, response } = await ativosService.getByCodCliente(codigo);
     return res.status(code).json(response);
   }
@@ -15,6 +15,18 @@ const getByCodAtivoOuCliente = async (req, res, next) => {
     return res.status(code).json(response);
   }
 };
+
+// Modificar a partir daqui e colocar 2 no de cima
+
+/* getByCodAtivoOuCliente = async (req, res, next) => {
+  const { codigo } = req.params;
+  const isCodAtivo = await ativosService.isCodAtivoOuCliente(codigo);
+  if (isCodAtivo) {
+
+  }
+  if (isCliente)
+} */
+
 
 module.exports = {
   getByCodAtivoOuCliente,
