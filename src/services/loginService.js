e0d48f2042f4e161b99cc58d5d033a67d6b9dd90
carolcabralm <carolcabralm@msn.com>
2022-07-22 09:49:28 -0300

    refactor: incluindo StatusCodes em LoginService

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -1,14 +1,16 @@
 const loginModel = require('../models/loginModel');
 const { token } = require('../middlewares/token');
+// import { ReasonPhrases, StatusCodes } from 'http-status-codes';
+const { StatusCodes } = require('http-status-codes');
 
 const login = async (codCliente, senha) => {
   const users = await loginModel.login()
   const findUser= users.find((user) => user.codCliente === codCliente && user.senha === senha);
-  if (!findUser) return { code: 404, response: { message: 'Cliente não encontrado' } };
+  if (!findUser) return { code: StatusCodes.NOT_FOUND, response: { message: 'Usuário ou senha incorretos. Favor verificar seu dados.' } };
 
   const payload = { codCliente, senha };
   const genToken = token(payload);
-  return { code: 200, response: { message: 'Login realizado com sucesso', token: genToken } };
+  return { code: StatusCodes.OK, response: { message: 'Login realizado com sucesso', token: genToken } };
 }
 
 module.exports = {
