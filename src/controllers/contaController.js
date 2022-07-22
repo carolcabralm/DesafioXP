0dd68c35858a5d953717a9b96d1a27e4af4cc457
carolcabralm <carolcabralm@msn.com>
2022-07-22 15:16:45 -0300

    style: corrigindo problemas de lint

--- src/controllers/contaController.js
+++ src/controllers/contaController.js
@@ -1,18 +1,18 @@
 const contaService = require('../services/contaService');
 
-const deposito = async (req, res, next) => {
+const deposito = async (req, res) => {
   const { codCliente, valor } = req.body;
   const { code, response } = await contaService.deposito(codCliente, valor);
   return res.status(code).json(response);
 }
 
-const saque = async (req, res, next) => {
+const saque = async (req, res) => {
   const { codCliente, valor } = req.body;
   const { code, response } = await contaService.saque(codCliente, valor);
   return res.status(code).json(response);
 }
 
-const saldo = async (req, res, next) => {
+const saldo = async (req, res) => {
   const { codCliente } = req.params;
   const { code, response } = await contaService.saldo(codCliente);
   return res.status(code).json({codCliente, ...response});
--- src/controllers/investimentosController.js
+++ src/controllers/investimentosController.js
@@ -1,12 +1,12 @@
 const investimentosService = require('../services/investimentoService');
 
-const investimentosComprar = async (req, res, next) => {
+const investimentosComprar = async (req, res) => {
   const { codCliente, codAtivo, qtdeAtivo } = req.body;
   const { code, response } = await investimentosService.ativosComprar(codCliente, codAtivo, qtdeAtivo);
   return res.status(code).json(response);
 }
 
-const investimentosVender = async (req, res, next) => {
+const investimentosVender = async (req, res) => {
   const { codCliente, codAtivo, qtdeAtivo } = req.body;
   const { code, response } = await investimentosService.ativosVender(codCliente, codAtivo, qtdeAtivo);
   return res.status(code).json(response);
